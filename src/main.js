35cc5bb316d83b7afecb7f512cf931d431e0239c
Evan Aki <evanakihito@outlook.com>
2022-09-21 02:35:10 +0800

    feat: 项目最佳fcp

--- src/main.js
+++ src/main.js
@@ -2,12 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-// import ElementUI from 'element-ui'
+import ElementUI from 'element-ui'
 // import { Button } from 'element-ui'
-// import 'element-ui/lib/theme-chalk/index.css'
+import 'element-ui/lib/theme-chalk/index.css'
 Vue.config.productionTip = false
 // Vue.use(Button)
-// Vue.use(ElementUI)
+Vue.use(ElementUI)
 
 new Vue({
   router,
