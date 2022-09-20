1dda91a07b83e1e5b03fefb6f7a41c810b308deb
liuhan <evanakihito@outlook.com>
2022-09-20 16:44:02 +0800

    feat: 项目初始化，配置所需依赖

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/"><el-button type="primary">Home</el-button></router-link> |
+      <router-link to="/about"><el-button type="primary">About</el-button></router-link>
     </nav>
     <router-view/>
   </div>
