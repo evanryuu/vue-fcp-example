02db1a299b144c8a8c9551658e3f94dd55123e43
liuhan <evanakihito@outlook.com>
2022-09-20 18:29:37 +0800

    feat: 添加按需加载

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,19 @@
 const { defineConfig } = require('@vue/cli-service')
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
+const IS_PROD = process.env.NODE_ENV === 'production'
 module.exports = defineConfig({
   transpileDependencies: true,
   chainWebpack: (config) => {
-    config.plugin('webpack-bundle-analyzer')
-      .use(new BundleAnalyzerPlugin(
-        {
-          analyzerMode: 'static', // 可选值有server static disabled
-          generateStatsFile: false,
-          statsOptions: { source: false },
-          openAnalyzer: false
-        }
-      ))
+    if (IS_PROD) {
+      config.plugin('webpack-bundle-analyzer')
+        .use(new BundleAnalyzerPlugin(
+          {
+            analyzerMode: 'static', // 可选值有server static disabled
+            generateStatsFile: false,
+            statsOptions: { source: false },
+            openAnalyzer: false
+          }
+        ))
+    }
   }
 })
