bb404fb3bde96776e191a92f1c99a7e956de6260
Evan Aki <evanakihito@outlook.com>
2022-09-21 01:42:35 +0800

    feat: cdn引入外部依赖，减小打包体积

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,14 @@
 module.exports = {
   presets: [
     ['@babel/preset-env', { modules: false }]
-  ],
-  plugins: [
-    [
-      'component',
-      {
-        libraryName: 'element-ui',
-        styleLibraryName: 'theme-chalk'
-      }
-    ]
   ]
+  // plugins: [
+  //   [
+  //     'component',
+  //     {
+  //       libraryName: 'element-ui',
+  //       styleLibraryName: 'theme-chalk'
+  //     }
+  //   ]
+  // ]
 }
--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,28 @@
+/* eslint-disable */
 const { defineConfig } = require('@vue/cli-service')
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 const IS_PROD = process.env.NODE_ENV === 'production'
+let externals = {}
+if (IS_PROD) {
+  externals = {
+    'vue': 'Vue', // CDN 的 Element 依赖全局变量 Vue， 所以 Vue 也需要使用 CDN 引入
+    'vue-router': 'VueRouter',
+    'vuex': 'Vuex',
+    'element-ui': 'ELEMENT', // 不去 node_modules 中找，而是去找 全局变量 ELEMENT
+    'axios': 'axios'
+  }
+}
 module.exports = defineConfig({
   transpileDependencies: true,
+  // configureWebpack: {
+  //   externals: {
+  //     'vue': 'Vue', // CDN 的 Element 依赖全局变量 Vue， 所以 Vue 也需要使用 CDN 引入
+  //     'vue-router': 'VueRouter',
+  //     'vuex': 'Vuex',
+  //     'element-ui': 'ELEMENT', // 不去 node_modules 中找，而是去找 全局变量 ELEMENT
+  //     'axios': 'axios'
+  //   }
+  // },
   chainWebpack: (config) => {
     if (IS_PROD) {
       config.plugin('webpack-bundle-analyzer')
@@ -15,5 +35,12 @@ module.exports = defineConfig({
           }
         ))
     }
+    config.externals({
+      'vue': 'Vue', // CDN 的 Element 依赖全局变量 Vue， 所以 Vue 也需要使用 CDN 引入
+      'vue-router': 'VueRouter',
+      'vuex': 'Vuex',
+      'element-ui': 'ELEMENT', // 不去 node_modules 中找，而是去找 全局变量 ELEMENT
+      'axios': 'axios'
+    })
   }
 })
